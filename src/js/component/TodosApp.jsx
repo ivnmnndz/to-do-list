f28e5070aad16fe937209d759f022e73a3aa7eb1
ivnmnndz <12gues@gmail.com>
2021-11-04 18:07:00 +0000

    changes in styles and general naming

--- src/js/component/TodosApp.jsx
+++ src/js/component/TodosApp.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 
-export default function Home() {
-	const [list, setList] = useState(["test", "values"]);
+export default function TodosApp() {
+	const [list, setList] = useState([]);
 	const [content, setContent] = useState("");
 
 	return (
 		<div className="container-fluid">
 			<h1 className="row mt-5 justify-content-center">To-Do List</h1>
-			<div className="col-lg-6 p-3 m-auto todoList">
+			<div className="col-lg-6 p-3 m-auto wrapper">
 				<input
 					className="row m-auto w-100"
 					placeholder="Add Activity"
@@ -15,24 +15,26 @@ export default function Home() {
 					onChange={event => setContent(event.target.value)}
 					onKeyPress={event => {
 						if (event.key === "Enter" && content !== "") {
-							setList(list.concat(content));
+							setList([...list, content]);
 							setContent("");
 						}
 					}}
 				/>
-				{list.map((item, index) => {
-					return (
-						<div className="row m-auto listItem" key={index}>
-							{item}
-							<button
-								onClick={() =>
-									setList(list.filter(e => e !== item))
-								}
-								type="button"
-								className="ms-auto align-self-center btn btn-close"></button>
-						</div>
-					);
-				})}
+				<ul className="todoList">
+					{list.map((item, index) => {
+						return (
+							<li className="row m-auto listItem" key={index}>
+								{item}
+								<button
+									onClick={() =>
+										setList(list.filter(e => e !== item))
+									}
+									type="button"
+									className="ms-auto align-self-center btn btn-close"></button>
+							</li>
+						);
+					})}
+				</ul>
 				<div className="row m-auto">
 					<span className="reminder text-muted">
 						{list.length} items left
